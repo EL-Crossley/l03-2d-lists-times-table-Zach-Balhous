5eb885b01736f84c1296b14062301cee4c32003f
Zach-Balhous <zacharybalhous@gmail.com>
2024-10-21 13:50:59 -0400

    Finished the assignment

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 # Put your function here
-
+def timesTable(nums):
+    nums = nums + 1
+    combinedArr = []
+    
+    for i in range(nums - 1):
+        arr = []
+        nums = nums - 1
+        multiplier = 0
+        for i in range(0, 5):
+            multiplier +=1 
+            arr.append(nums * (multiplier))
+            #print(arr)
+        combinedArr.append(arr)
+        
+      
+    return(combinedArr)
 
 # testing
 nums = int(input())
